01c71eed9f88df234502b90567bc6b2cd87d98db
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-10-15 00:24:17 +0400

    home banner

--- src/components/home/HomeBanner.jsx
+++ src/components/home/HomeBanner.jsx
@@ -1,8 +1,8 @@
 import "./HomeBannerStyle.scss";
 import womenFashion from "../../assets/category1.webp";
 import menFashion from "../../assets/category2.webp";
-import kidFashion from "../../assets/category3.webp";
 import cosmetics from "../../assets/category4.webp";
+import kidFashion from "../../assets/category3.webp";
 import accessories from "../../assets/category5.webp";
 
 const HomeBanner = () => {
@@ -10,20 +10,43 @@ const HomeBanner = () => {
     <div className="banner-wrapper">
       <div className="img-wrapper">
         <div className="large-img">
-            <img src={womenFashion} alt="women-fashion"/>
-            <div className="women-fashion">
-                <h3>Women's Fashion</h3>
-                <p>Sitamet, consectetur adipiscing elit, sed do eiusmod tempor incidid-unt labore edolore magna aliquapendisse ultrices gravida.</p>
-                <p>Shop Now</p>
-            </div>
+          <img src={womenFashion} alt="women-fashion" />
+          <div className="women-fashion">
+            <h3>Women's Fashion</h3>
+            <span>
+              Sitamet, consectetur adipiscing elit, sed do eiusmod tempor
+              incidid-unt labore edolore magna aliquapendisse ultrices gravida.
+            </span>
+            <p className="shop-now">Shop Now</p>
+          </div>
         </div>
         <div className="sml-img">
-            <img src={menFashion} alt="fashion"/>
-            <img src={kidFashion} alt="fashion"/>
+          <img src={menFashion} alt="fashion" />
+          <img src={cosmetics} alt="fashion" />
+          <div className="men-fashion">
+            <h3>Men's Fashion</h3>
+            <span>358 items</span>
+            <p className="shop-now">Shop Now</p>
+          </div>
+          <div className="cosmetic-fashion">
+            <h3>Cosmetics</h3>
+            <span>159 items</span>
+            <p className="shop-now">Shop Now</p>
+          </div>
         </div>
-        <div  className="sml-img">
-            <img src={cosmetics} alt="fashion"/>
-            <img src={accessories} alt="fashion"/>
+        <div className="sml-img">
+          <img src={kidFashion} alt="fashion" />
+          <img src={accessories} alt="fashion" />
+          <div className="men-fashion">
+            <h3>Kid's Fashion</h3>
+            <span>233 items</span>
+            <p className="shop-now">Shop Now</p>
+          </div>
+          <div className="cosmetic-fashion">
+            <h3>Accessroies</h3>
+            <span>775 items</span>
+            <p className="shop-now">Shop Now</p>
+          </div>
         </div>
       </div>
     </div>
