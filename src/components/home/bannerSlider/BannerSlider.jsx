c24afef0e061a8f0bb9f7d4029b5f75493c84283
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-10-15 15:12:25 +0400

    banner slider

--- src/components/home/bannerSlider/BannerSlider.jsx
+++ src/components/home/bannerSlider/BannerSlider.jsx
@@ -1,12 +1,18 @@
 import "./BannerSlyderStyle.scss";
+import banner from "../../../assets/slider-banner.webp";
 
 
-const BannerSlider = ()=>{
-    return (
-        <div className="banner-back">
-          
-        </div>
-    )
-}
+const BannerSlider = () => {
+  return (
+    <div className="banner-back">
+        <img src={banner} alt="banner"/>
+      <div className="text-container">
+        <p className="chloe">THE CHLOE COLLECTION</p>
+        <h3 className="project">The Project Jacket</h3>
+        <p className="shop-now">SHOP NOW</p>
+      </div>
+    </div>
+  );
+};
 
-export {BannerSlider}
+export { BannerSlider };
