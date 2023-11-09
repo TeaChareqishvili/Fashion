3cf7aaff76ff62f5b874fbb373456e40d6a4e513
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-11-09 15:25:45 +0400

    price range done

--- src/components/shopp/PriceRange.jsx
+++ src/components/shopp/PriceRange.jsx
@@ -1,10 +1,66 @@
-import "./ShopStyle.scss";
+import "./PriceRange.scss";
+import { useState, useEffect, useRef } from "react";
 
+const PriceRange = () => {
+  const [minValue, setMinValue] = useState(0);
+  const [maxValue, setMaxValue] = useState(10000);
 
-const PriceRange = ()=>{
-    return (
-        <div>price</div>
-    )
-}
+  const rangeRef = useRef(null);
 
-export {PriceRange}
+  const [range, setRange] = useState(true);
+
+  useEffect(() => {
+    const handleVisible = (e) => {
+      if (rangeRef.current && rangeRef.current.contains(e.target)) {
+        setRange((prevState) => !prevState);
+      }
+    };
+
+    document.addEventListener("click", handleVisible);
+
+    return () => {
+      document.removeEventListener("click", handleVisible);
+    };
+  }, [rangeRef]);
+
+  return (
+    <div className="price-wrapper">
+      <div className="category-title">
+        <h2>Price range</h2>
+      </div>
+      {range && (
+        <div>
+          <div className="progress-range">
+            <div className="range">
+              <div className="progress"></div>
+            </div>
+            <div className="input-range">
+              <input
+                type="range"
+                className="progress-min"
+                min="0"
+                max="10000"
+                onChange={(e) => setMinValue(e.target.value)}
+              />
+              <input
+                type="range"
+                className="progress-max"
+                min="0"
+                max="10000"
+                onChange={(e) => setMaxValue(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="price-range">
+            <p>
+              Price {minValue} - {maxValue}
+            </p>
+            <button className="apply-btn">FILTER</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export { PriceRange };
