5852e76fb1a0f2d7810944bc5306533d1101c8cc
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-11-15 14:30:22 +0400

    favoriteitems component style

--- src/components/favorite/FavoriteItems.jsx
+++ src/components/favorite/FavoriteItems.jsx
@@ -1,24 +1,46 @@
 import "./FavoriteStyle.scss";
 import { useAddFavorites } from "../hook/saveItems";
+import { VscChromeClose } from "react-icons/vsc";
+import { FaCartArrowDown } from "react-icons/fa";
 
-const FavoriteItems =()=>{
+const FavoriteItems = () => {
+  const { addFavorite } = useAddFavorites();
 
-    const {addFavorite} = useAddFavorites()
+  console.log("favorute", addFavorite);
 
-console.log("favorute", addFavorite)
-   
-
-    return(
-        <div className="favorite-main-wrapper">
-         <div className="favorite-items">
-            <div>
-            <p>Favorite Items</p>
-            <p>Total <span>{addFavorite.length}</span></p>
-            </div>
-            <div className="border"></div>
-         </div>
+  return (
+    <div className="favorite-main-wrapper">
+      <div className="favorite-items">
+        <div>
+          <p className="title">Favorite Items</p>
+          <p className="title">
+            Total <span>{addFavorite.length}</span>
+          </p>
         </div>
-    )
-}
+        <div className="border"></div>
+        {addFavorite &&
+          addFavorite.map((item, id) => (
+            <>
+              <div key={id} className="favorite-chosen">
+                <div className="each-item">
+                  <img src={item.image} alt="item" />
+                  <div className="about-favorite">
+                    <p className="title">{item.description}</p>
+                    <p>{item.about}</p>
+                    <p className="price">${item.price}</p>
+                  </div>
+                  <div className="delete-cart">
+                    <FaCartArrowDown className="cart"/>
+                    <VscChromeClose className="close" />
+                  </div>
+                </div>
+              </div>
+              <div className="border"></div>
+            </>
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export {FavoriteItems}
+export { FavoriteItems };
